11e9068a876e4cf7f7812d8b7149f83287a1e628
Konstantin <altwebga@gmail.com>
2024-10-11 21:40:23 +0700

    new setting

--- Dockerfile
+++ Dockerfile
@@ -5,14 +5,14 @@ ARG NODE_ENV=development
 ENV NODE_ENV=${NODE_ENV}
 
 WORKDIR /opt/
-COPY package.json yarn.lock ./
-RUN yarn config set network-timeout 600000 -g && yarn install
+COPY package.json package-lock.json ./
+RUN npm config set fetch-retry-maxtimeout 600000 -g && npm install
 
 WORKDIR /opt/app
 COPY . .
 ENV PATH /opt/node_modules/.bin:$PATH
 RUN chown -R node:node /opt/app
 USER node
-RUN ["yarn", "build"]
+RUN ["npm", "run", "build"]
 EXPOSE 1337
-CMD ["yarn", "develop"]
+CMD ["npm", "run", "develop"]
--- config/env/development/database.ts
+++ config/env/development/database.ts
@@ -2,11 +2,11 @@ export default ({ env }) => ({
 	connection: {
 		client: 'postgres',
 		connection: {
-		host: env('DATABASE_HOST', 'localhost'),
+		host: env('DATABASE_HOST'),
 			port: env.int('DATABASE_PORT', 5432),
-			database: env('DATABASE_NAME', 'strapi'),
-			user: env('DATABASE_USERNAME', 'strapi'),
-			password: env('DATABASE_PASSWORD', 'v+J~6GF^6AGZFyk6Phdm'),
+			database: env('DATABASE_NAME'),
+			user: env('DATABASE_USERNAME'),
+			password: env('DATABASE_PASSWORD'),
 			ssl: env.bool('DATABASE_SSL', false)
 		}
 	}
