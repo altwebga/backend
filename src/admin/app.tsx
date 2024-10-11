497bc0785178f6a1094306a1db55abcd6227b879
Konstantin <altwebga@gmail.com>
2024-10-11 05:54:53 +0000

    add: contaent type

---
+++ src/admin/app.tsx
@@ -0,0 +1,11 @@
+import type { StrapiApp } from '@strapi/strapi/admin';
+
+export default {
+  config: {
+    locales: ['ru'],
+    notifications: { releases: false },
+  },
+  bootstrap(app: StrapiApp) {
+    console.log(app);
+  },
+};
