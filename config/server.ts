7392891d3bcb5623c05cc0363a482f4f02b4377c
Konstantin <altwebga@gmail.com>
2025-09-10 16:47:15 +0300

    Конфигурация сервера

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,10 @@
+// config/server.ts
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "127.0.0.1"), // в проде лучше 127.0.0.1 (за nginx)
+  port: env.int("PORT", 1337),
+  url: env("URL", "https://digital-env.ru"), // абсолютные ссылки (админка, письма, OAuth-коллбеки)
+  proxy: env.bool("PROXY", true), // доверять X-Forwarded-* от nginx
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
 });
