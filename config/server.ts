9e553234d8fd67198a47989c182f169557faa1d5
Konstantin <altwebga@gmail.com>
2025-09-10 17:06:58 +0300

    vite config

--- config/server.ts
+++ config/server.ts
@@ -2,7 +2,7 @@
 export default ({ env }) => ({
   host: env("HOST", "127.0.0.1"), // в проде лучше 127.0.0.1 (за nginx)
   port: env.int("PORT", 1337),
-  url: env("URL", "https://digital-env.ru"), // абсолютные ссылки (админка, письма, OAuth-коллбеки)
+  url: env("URL", "http://localhost:1337"), // абсолютные ссылки (админка, письма, OAuth-коллбеки)
   proxy: env.bool("PROXY", true), // доверять X-Forwarded-* от nginx
   app: {
     keys: env.array("APP_KEYS"),
